af6366754842382c511a5fc1d2065b3478bae0bf
LukeLage <100864023+LukeLage@users.noreply.github.com>
2025-01-17 17:42:51 -0300

    Add files via upload

---
+++ Variaveis e Funções/Função2.js
@@ -0,0 +1,7 @@
+function subtracao(a, b) {
+    return a - b;
+}
+
+const num1 = 5;
+const num2 = 3; 
+console.log(`[${subtracao.name}] entre ${num1} e ${num2}:`, subtracao(num1, num2));
---
+++ Variaveis e Funções/Função4.js
@@ -0,0 +1,7 @@
+function divisao(a, b) {
+    return a / b;
+}
+
+const num1 = 5;
+const num2 = 3; 
+console.log(`[${divisao.name}] entre ${num1} e ${num2}:`, divisao(num1, num2));
